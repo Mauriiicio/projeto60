cbda0a657181f1345e4b84e2187c0c3f35b0b929
Mauriiicio <mauricio1desouza@gmail.com>
2026-04-28 21:31:34 -0400

    comentario legal se nao o professor reprova

--- teste.py
+++ teste.py
@@ -2,3 +2,8 @@ nome = "Rafale"
 idade = 28
 
 print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
+print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
+print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
+print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
+print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
+print("Ola meu nome é ", idade, "Tenho ", nome, "anos.")
